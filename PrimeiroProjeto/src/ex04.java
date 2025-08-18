0c7ad249cb8c63b55f772e464f405f67433ce30d
Rafael Nestor <rafaelnestor01@gmail.com>
2025-08-18 12:40:06 -0300

    ex04 feito

---
+++ PrimeiroProjeto/src/ex04.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class ex04 {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora do seu sistema e: ");
+        System.out.println(relogio.toString());
+    }
+}
